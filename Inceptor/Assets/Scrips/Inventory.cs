74cc170ff6e4ec6a4b09f12fc80abeea582435c9
Czany <Jakub_12@onet.pl>
2019-03-29 19:58:46 +0100

    Inventor

---
+++ Inceptor/Assets/Scrips/Inventory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private Canvas inventory;
+
+    void Start()
+    {
+        inventory = (Canvas)GetComponent<Canvas>();
+        inventory.enabled = false;
+    }
+
+    void Update()
+    {
+        
+        if(Input.GetKeyUp(KeyCode.I))
+        {
+            inventory.enabled = !inventory.enabled;
+            Cursor.visible = !inventory.enabled;
+
+            if(inventory.enabled)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+
+    }
+}
