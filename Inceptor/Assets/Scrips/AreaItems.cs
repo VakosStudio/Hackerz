4d91958748d44042f9538d857a3244faa9aea57f
Czany <Jakub_12@onet.pl>
2019-03-29 21:45:34 +0100

    Inventory

---
+++ Inceptor/Assets/Scrips/AreaItems.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AreaItems : MonoBehaviour, IDropHandler
+{
+    public ItemsType type = ItemsType.weapon;
+    public GameObject ObjectInventory;
+
+    public int maxItems;
+
+    private Transform trans;
+
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + "upuszczono w" + gameObject.name);
+
+        InventorElements d = eventData.pointerDrag.GetComponent<InventorElements>();
+        if (d != null)
+        {
+            if (type == d.type)
+            {
+                Debug.Log(trans.childCount);
+                if (trans.childCount < maxItems)
+                {
+                    d.setWindows(trans);
+                }
+                else
+                {
+                    Transform elem = transform.GetChild(0);
+                    elem.SetParent(ObjectInventory.transform);
+                    d.setWindows(trans);
+                }
+            }
+            else if (type == ItemsType.weapon)
+            {
+                d.setWindows(trans);
+            }
+        }
+    }
+}
