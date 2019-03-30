efda7bacaa2eaf8219bff698faf5599d32440544
Czany <Jakub_12@onet.pl>
2019-03-30 13:58:59 +0100

    Koenic

---
+++ GierkaKoniec/Assets/Scrips/Add.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Add : MonoBehaviour
+{
+
+    public Vector3 delta;
+    Vector3 startPosition;
+    public GameObject Fance;
+    public GameObject Cube;
+    collectingmaterials col;
+
+
+    void Start()
+    {
+        startPosition = gameObject.transform.position;
+    }
+
+
+    void Update()
+    {
+
+        Physics.Raycast(transform.position, Vector3.forward, 3f);
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (0 == 0)
+            {
+                Instantiate(Fance, Cube.transform.position, Quaternion.identity);
+                col.woods -= 0;
+            }
+            else
+            {
+                Debug.Log("Nie zbudujesz");
+            }
+        }
+
+    }
+}
---
+++ GierkaKoniec/Assets/Scrips/Creafting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Creafting : MonoBehaviour
+{
+    collectingmaterials col;
+    public GameObject axe;
+    public Button button; 
+
+    public void OnMouseDown()
+    {
+        creafting();
+    }
+
+    /*public void OnEnable()
+    {
+        button.onClick.AddListener(delegate { creafting(); });
+    }*/
+
+    public void creafting()
+    {
+        Debug.Log("eeeee");
+        if (gameObject.tag == "Axe")
+        {
+            if (col.woods >= 1 && col.sticks >=3)
+            {
+                Instantiate(axe, transform.position, Quaternion.identity);
+            }
+        }
+
+    }
+}
